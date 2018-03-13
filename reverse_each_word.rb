a22061f4e55fa5d2965519dfa36e74a348397ad2
Jamal Gelin <gelin.jamal@gmail.com>
2018-03-13 04:45:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
 def reverse_each_word(sentence)
-    reversed = sentence.split
+    reversed = sentence.split(" ")
   word.reverse
-end
-
-def reverse_each_word()
-    .collect do ||
-    puts
-  end
 end
